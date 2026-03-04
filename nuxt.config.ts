2bc2fd44abb419b61322cbfde0401c35cbb2f03d
QQuispe <kevinq1@umbc.edu>
2026-03-03 21:18:50 -0500

    devtools logic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.DEV_MODE === 'true' || process.env.NODE_ENV === 'development' },
 
   modules: ['@nuxt/icon'],
 
